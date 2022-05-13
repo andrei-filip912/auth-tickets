8d2c95496629047ca35c22c7a00a44e24dce02c5
andrei-filip912 <filip.andrei912@gmail.com>
2022-05-13 17:21:57 +0200

    copy from hdd

---
+++ src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/users/currentuser',  (req, res) => {
+
+    res.send('Hello there!')
+ });
+
+export {router as currentUserRouter};
