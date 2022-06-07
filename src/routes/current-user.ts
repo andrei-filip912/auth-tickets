c436108ded20ea9aed1e1242936d75da2ca17ecf
andrei-filip912 <filip.andrei912@gmail.com>
2022-06-07 02:25:01 +0200

    moved logic into npm package and imported it, updated imports

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@frst-ticket-app/common';
 
 const router = express.Router();
 
